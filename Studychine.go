c30634cee37829128047f494af2c08cb170c2edb
QiaoXun <821465029@qq.com>
2017-05-14 16:37:49 +0800

    New

--- Studychine.go
+++ Studychine.go
@@ -5,22 +5,24 @@ import (
 	"net/http"
 	"strconv"
 	"github.com/Maoyis/GO-Study"
-	//logging "github.com/op/go-logging"
+	"github.com/op/go-logging"
+	"os"
 )
 
 func heartbreakerHandler(w http.ResponseWriter, req *http.Request) {
+	logger.Infof("Meet heartbreaker from %s", req.Host)
 	io.WriteString(w, GO_Study.Study())
 }
 
 var LISTENING_PORT = 1024
-//var logger = logging.MustGetLogger("hugmachine.log")
+var logger = logging.MustGetLogger("hugmachine.log")
 
 func main() {
-	//logging.NewLogBackend(os.Stderr, "", 0)
+	logging.NewLogBackend(os.Stderr, "", 0)
 	http.HandleFunc("/study", heartbreakerHandler)
-	//logger.Infof("Listening on port %d", LISTENING_PORT)
+	logger.Infof("Listening on port %d", LISTENING_PORT)
 	err := http.ListenAndServe("0.0.0.0:"+strconv.Itoa(LISTENING_PORT), nil)
 	if err != nil {
-		//logger.Fatal("ListenAndServe: " + err.Error())
+		logger.Fatal("ListenAndServe: " + err.Error())
 	}
 }
